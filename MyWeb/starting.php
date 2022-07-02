0b6c1b7b87be1c8a828e90a110096fd0081dc920
Adeyemi Oluwagbenga Daniel <88782647+DanielGad@users.noreply.github.com>
2022-07-02 01:11:26 +0100

    Add files via upload

---
+++ MyWeb/starting.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>GETTING STARTED WITH BRACKETS</title>
+        <meta name="description" content="An interactive getting started guide for Brackets.">
+        <link rel="stylesheet" href="Home.css">
+    </head>
+            <?php include 'nav.php' ?>
+        <body style="background-color: burlywood">
+                <div>
+                    <h1 style="background-color: darkgray"> CREATION OF THE EARTH AND ITS FULLNESS</h1>
+                    The bible says in psalm 24 verse 1 "the earth is the lord and its <span>fullness</span> thereof, the world and those who dwell there in"<br style="background-color: aqua">
+                    Also in Genesis 1:1 it says "in the beginning, God created the heaven and the earth."<br>
+                </div>
+        
+                <main>
+                    <img src="../images/testing image.jpg" />
+                    <br><br><br>
+                    <!-- i added 3 breaks so that the next line will be spaced from the image very well
+                    i added 3 breaks so that the next line will be spaced from the image very well
+                    i added 3 breaks so that the next line will be spaced from the image very well -->
+                    Just added new things <br>yet another <span>addition</span>
+        
+                </main>
+                
+        </body>
+       
+        
+        
+
+  
+</html>
